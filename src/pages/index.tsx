6e3eb6c260c436f1edb3bf2264428912777944e9
Matt Bremmers <bremmdev@gmail.com>
2023-02-01 22:31:58 +0100

    refactor: replace create callers by ssg helpers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,45 @@
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import type { InferGetStaticPropsType } from "next";
 import Bio from "../components/Bio/Bio";
 import TechSection from "../components/tech/TechSection";
 import { trpc } from "../utils/trpc";
 import { appRouter } from "../server/trpc/router/_app";
-import { prisma } from "../server/db/client";
 import superjson from "superjson";
 import ProjectSection from "../components/project/ProjectSection";
 import Header from "../components/Layout/Header";
 import QuickLinks from "../components/Layout/QuickLinks";
-import type { Project, Tech } from "@prisma/client";
+import { createContextInner } from "../server/trpc/context";
 
-const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home = () => {
   //queries will be fetched instantly because of the cached response from the server
   const { data: tech } = trpc.tech.findAll.useQuery();
   const { data: projects } = trpc.project.findAll.useQuery();
-
-  const prop: { tech: Tech[], projects: Project[]} = superjson.deserialize(props.superjsonData)
 
   return (
     <div className="mx-auto flex max-w-7xl flex-col items-center gap-8 py-20 px-4 text-center text-white sm:gap-12 sm:py-28 sm:px-8">
       <Header />
       <QuickLinks />
       <Bio />
-      {prop.projects && <ProjectSection projects={prop.projects} />}
-      {prop.tech && <TechSection tech={prop.tech} />}
+      {projects && <ProjectSection projects={projects} />}
+      {tech && <TechSection tech={tech} />}
     </div>
   );
 };
 export default Home;
 
 export async function getStaticProps() {
-  // const ssg = await createProxySSGHelpers({
-  //   router: appRouter,
-  //   ctx: {
-  //     prisma: prisma,
-  //   },
-  //   transformer: superjson,
-  // });
+  const ssg = await createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContextInner({}),
+    transformer: superjson,
+  });
 
-  // //prefetch data for ssg
-  // await ssg.project.findAll.fetch();
-  // await ssg.tech.findAll.fetch();
-
-  // return {
-  //   props: {
-  //     trpcState: ssg.dehydrate(),
-  //   },
-  //   revalidate: 10,
-  // };
-
-  const caller = appRouter.createCaller({prisma})
-  const projects = await caller.project.findAll()
-  const tech = await caller.tech.findAll()
-
-  const superjsonData = superjson.serialize({
-    projects,
-    tech
-  })
-
+  //prefetch data for ssg
+  await ssg.project.findAll.prefetch();
+  await ssg.tech.findAll.prefetch();
 
   return {
     props: {
-     superjsonData
+      trpcState: ssg.dehydrate(),
     },
     revalidate: 10,
   };
