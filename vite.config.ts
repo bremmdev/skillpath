a9bccbc97ff02c0a7eed920af75d4879e939e514
bremmdev <bremmdev@gmail.com>
2026-06-20 14:19:17 +0200

    setup tanstack router

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  build: {
+    outDir: 'dist-ui',
+  },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: './src/ui/routes',
+      generatedRouteTree: './src/ui/routeTree.gen.ts',
+    }),
+    viteReact(),
+  ],
+})
+
+export default config
